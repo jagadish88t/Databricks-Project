74035f0f8ec90d6f43dc4bc7c427b9631879907b
jagadish88t <107781655+jagadish88t@users.noreply.github.com>
2024-02-05 16:26:22 -0500

    testing

--- main.tf
+++ main.tf
@@ -51,6 +51,7 @@ data "azurerm_databricks_workspace" "databricks_data" {
 resource "databricks_secret_scope" "databricks_secret" {
   name = "terraform-test-scope"
   initial_manage_principal = "users"
+  backend_type = "AZURE_KEYVAULT"
   keyvault_metadata {
     resource_id = azurerm_key_vault.kv_databricks.id
     dns_name = azurerm_key_vault.kv_databricks.vault_uri
