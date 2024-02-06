871590c106a57cd4ca07e2b474c383481371b53d
jagadish88t <107781655+jagadish88t@users.noreply.github.com>
2024-02-05 19:28:49 -0500

    remove secrets

--- provider.tf
+++ provider.tf
@@ -19,9 +19,6 @@ terraform {
     client_id = "e21fc4cc-3424-40d2-af2d-b86dc784e727"
     client_secret = "23K8Q~JEv6lrnUYHvXiyJtnIvHIDXZQRTIklybmb"
     subscription_id = "63bd2eef-d99c-4b36-a3b0-b89c12000aa7"
-    #$tenantId = "4a1c61bf-11d9-4bcc-8493-2d40bb0e45b7"
-    #$clientId = "e21fc4cc-3424-40d2-af2d-b86dc784e727"
-    #$clientSecret = "23K8Q~JEv6lrnUYHvXiyJtnIvHIDXZQRTIklybmb"
 
     resource_group_name = "Terraformfiles"
     storage_account_name = "tfstatefileslearning"
